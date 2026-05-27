666843a303bfd5913810d2a910ecb07741c28f14
Valentyn Deshel <desheval@fjfi.cvut.cz>
2026-05-27 12:30:00 +0200

    :tada: init
    # Conflicts:
    #	.gitignore

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    kotlin("jvm") version "2.3.0"
+    id("org.jetbrains.intellij.platform") version "2.5.0"
+}
+
+group = "valentyn.deshel"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+dependencies {
+    intellijPlatform {
+        intellijIdeaUltimate("2025.3.2")
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        id.set("valentyn.deshel.studylogger")
+        name.set("Study Integrity Logger")
+        version.set(project.version.toString())
+
+        ideaVersion {
+            sinceBuild.set("243")
+        }
+    }
+}
---
+++ src/main/kotlin/valentyn/deshel/studylogger/StudyPathFilter.kt
@@ -0,0 +1,4 @@
+package valentyn.deshel.studylogger
+
+class StudyPathFilter {
+}
