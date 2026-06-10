37356017ef861c2be044a9d169ca512c30663ad9
glancyF <valentindesel@gmail.com>
2026-06-10 19:28:19 +0200

    :construction: add session writer

--- src/main/kotlin/deshel/valentyn/studylogger/listener/StudyProjectActivityListener.kt
+++ src/main/kotlin/deshel/valentyn/studylogger/listener/StudyProjectActivityListener.kt
@@ -3,10 +3,12 @@ package deshel.valentyn.studylogger.listener
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
 import deshel.valentyn.studylogger.writer.StudyLogWriter
+import deshel.valentyn.studylogger.writer.StudySessionInfoWriter
 
 class StudyProjectActivityListener : ProjectManagerListener {
     override fun projectOpened(project: Project) {
         StudyLogWriter.logProjectEvent(project, "PROJECT_OPENED")
+        StudySessionInfoWriter.writeSessionInfo(project)
     }
 
     override fun projectClosing(project: Project) {
