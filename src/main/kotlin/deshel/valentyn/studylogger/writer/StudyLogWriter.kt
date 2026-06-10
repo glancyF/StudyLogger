399186fa485f0e3277ff10db337b1c8f27b9c8a0
glancyF <valentindesel@gmail.com>
2026-06-10 20:03:03 +0200

    :sparkles: add off/on button

--- src/main/kotlin/deshel/valentyn/studylogger/writer/StudyLogWriter.kt
+++ src/main/kotlin/deshel/valentyn/studylogger/writer/StudyLogWriter.kt
@@ -2,6 +2,7 @@ package deshel.valentyn.studylogger.writer
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import deshel.valentyn.studylogger.state.StudyLoggerState
 import org.jetbrains.jsonProtocol.EventType
 import java.nio.charset.StandardCharsets
 import java.nio.file.Files
@@ -55,6 +56,9 @@ object StudyLogWriter {
         }
     }
      fun logFileEvent(project: Project, eventType: String, file: VirtualFile) {
+         if (!StudyLoggerState.isEnabled()) {
+             return
+         }
         try {
             val projectBasePath = project.basePath ?: return
             val projectRoot = Path.of(projectBasePath)
@@ -94,6 +98,9 @@ object StudyLogWriter {
     }
 
     fun logProjectEvent(project: Project, eventType: String) {
+        if (!StudyLoggerState.isEnabled()) {
+            return
+        }
         try {
             val projectBasePath = project.basePath ?: return
 
@@ -108,6 +115,35 @@ object StudyLogWriter {
             val previousEventHash = getPreviousEventHash(logFile)
             val eventData = "$timestamp | $eventType | project=$projectName | prev_hash=$previousEventHash"
             val eventHash = calculateTextSha256(eventData)
+            val line = "$eventData | event_hash=$eventHash${System.lineSeparator()}"
+
+            Files.writeString(
+                logFile,
+                line,
+                StandardOpenOption.CREATE,
+                StandardOpenOption.APPEND
+            )
+        } catch (_: Exception) {
+            // log
+        }
+    }
+    fun logPluginStateEvent(project: Project, eventType: String) {
+        try {
+            val projectBasePath = project.basePath ?: return
+
+            val projectRoot = Path.of(projectBasePath)
+            val logDirectory = projectRoot.resolve(".study-log")
+            val logFile = logDirectory.resolve("file-events.log")
+
+            Files.createDirectories(logDirectory)
+
+            val timestamp = OffsetDateTime.now().format(formatter)
+
+            val previousEventHash = getPreviousEventHash(logFile)
+
+            val eventData = "$timestamp | $eventType | logger_enabled=${eventType == "PLUGIN_ENABLED"} | prev_hash=$previousEventHash"
+            val eventHash = calculateTextSha256(eventData)
+
             val line = "$eventData | event_hash=$eventHash${System.lineSeparator()}"
 
             Files.writeString(
